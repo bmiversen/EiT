ccfabf14ec7a205f04f49181e72a259d3ae93f9e
Hermann Villanger <hermannvillanger@gmail.com>
2019-02-20 12:59:45 +0100

    Testdata and App setup

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, { Component } from "react";
 import LeafletMap from "./components/map";
 import Sidebar from "./components/sidebar";
 import "./App.css";
+import testData from "./testdata.json";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { data: [] };
+  }
+  componentDidMount() {
+    this.readData(JSON.stringify(testData));
   }
 
+  readData = layers => {
+    let data = JSON.parse(layers);
+    this.setState({
+      data: data
+    });
+  };
+
   render() {
+    console.log(this.state.data);
     return (
       <React.Fragment>
         <Sidebar />
-        <LeafletMap />
+        <LeafletMap data={this.state.data} asPoints={true} />
       </React.Fragment>
     );
   }
